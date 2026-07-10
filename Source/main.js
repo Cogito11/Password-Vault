ac28d95e88722802a8588786f709322ec1df2aaf
Cogito11 <cdbishop2005@gmail.com>
2026-07-09 18:58:50 -0700

    Update main.js

--- Source/main.js
+++ Source/main.js
@@ -11,7 +11,7 @@ function getIconPath() {
   switch (process.platform) {
     case 'win32': return path.join(base, 'Password Vault Logo.ico');
     case 'linux': return path.join(base, 'icons', '256x256.png');
-    default: return path.join(base, 'PasswordVault.png');
+    default: return path.join(base, 'Password Vault Logo.icns');
   }
 }
 
